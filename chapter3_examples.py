7232912eb4ea208a67d976eb59297da8b19b77e2
Tim Sullivan <timsullivan97@gmail.com>
2018-12-11 19:00:35 -0800

    add more conditional statement examples

--- chapter3_examples.py
+++ chapter3_examples.py
@@ -4,3 +4,79 @@ for i in range(100):
 print\
 ("""This is a really really
 really long line of code.""")
+
+
+# Example of a conditional statement in pseudocode
+If (expression) Then
+    (codearea1)
+Else
+    (codearea2)
+
+
+# If-else statement
+home = "America"
+if home == "America":
+    print("Hello, America!")
+else:
+    print("Hello world!")
+    
+
+# If statement by itself
+home = "America"
+if home == "America":
+    print("Hello, America!")
+
+
+# Multiple if-statements in a row
+x = 2
+if x == 2:
+    print("The number is 2.")
+if x % 2 == 0:
+    print("The number is even!")
+if x % 2 != 0:
+    print("The number is odd.")
+
+
+# Nesting if-statements
+x = 10
+y = 11
+
+if x == 10:
+    if y == 11:
+        print(x + y)
+
+
+# Example of elif-statements
+home = "Thailand"
+if home == "Japan":
+    print("Hello, Japan!")
+elif home == "Thailand":
+    print("Hello, Thailand!")
+elif home == "India":
+    print("Hello, India!")
+elif home == "China":
+    print("Hello, China!")
+else:
+    print("Hello, world!")
+
+
+
+# Multiple if-statements and elif-statements in a row
+x = 100
+
+if x == 10:
+    print("10!")
+elif x == 20:
+    print("20!")
+else:
+    print("I don't know!")
+
+
+if x == 100:
+    print("x is 100!")
+
+
+if x % 2 == 0:
+    print("x is even!")
+else:
+    print("x is odd!")
