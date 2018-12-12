548134d2c2171635320f7a9883a6f1a1a68958e9
Tim Sullivan <timsullivan97@gmail.com>
2018-12-11 17:36:19 -0800

    add first challenge in book

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 03:13:28) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, world!")
+Hello, world!
+>>> print("Hello, world again!")
