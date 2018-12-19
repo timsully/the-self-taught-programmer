a8cb5a2c8dd02e078ed5a33b9ceb243011e97d0e
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 18:53:27 -0800

    add challenge 3

---
+++ Chapter 3. Introduction to Programming/challenge_03.py
@@ -0,0 +1,4 @@
+# Write a program that prints a message if a variable is less
+# than or equal to 10, another message if the variable is greater
+# than 10 but less than or equal to 25, and another message if the
+# variable is greater than 25.
