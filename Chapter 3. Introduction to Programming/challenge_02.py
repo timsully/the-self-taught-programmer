b9d185b7f2b61ca98e21bf48fcc657281b79bbb5
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 18:49:57 -0800

    add/complete challenge two

---
+++ Chapter 3. Introduction to Programming/challenge_02.py
@@ -0,0 +1,8 @@
+# Write a program that prints a message if a variable is less than 10
+# and different message if the variable is greater than or equal to 10.
+
+a = 5
+if a < 10:
+    print("Your variables assigned int value is less than ten playa.")
+else:
+    print("Your variable is greater than 10!")
