8b5615100850a0a1cc49a52c2e4dd73c0fe48777
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 18:27:32 -0800

    add compound statements and print statements with Python cause iz lit

--- chapter3_examples.py
+++ chapter3_examples.py
@@ -80,3 +80,29 @@ if x % 2 == 0:
     print("x is even!")
 else:
     print("x is odd!")
+
+# Compound statements
+x = 100
+if x == 10:
+    print("10!")
+elif x == 20:
+    print("20!")
+else:
+    print("I don't know!")
+
+if x == 100:
+    print("x is 100!")
+
+if x % 2 == 0:
+    print("x is even!")
+else:
+    print("x is odd!")
+
+
+# Spaces between statements do not affect code
+print("Michael")
+
+
+
+
+print("Jordan")
