09bf8bd741e46b4d1c695f5e0fb3030213cb8197
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 19:17:50 -0800

    add challenge 6

---
+++ Chapter 3. Introduction to Programming/challenge_06.py
@@ -0,0 +1,11 @@
+# Write a program with a variable age assigned to an integer that prints
+# different strings depending on what integer age is.
+
+age = 21
+
+if age == 18:
+    print("You're a young adult!")
+elif age == 21:
+    print("You're a young adult and can drink now!")
+else:
+    print("It's all downhill from here.")
