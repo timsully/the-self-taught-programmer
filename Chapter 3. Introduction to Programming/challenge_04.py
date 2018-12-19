6169f53410b01c68e71e47b37c1ddc9b4799acac
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 19:06:50 -0800

    complete exercise 4

---
+++ Chapter 3. Introduction to Programming/challenge_04.py
@@ -0,0 +1,9 @@
+# Create a program that divides two variables and prints the remainder
+a = 5
+b = 2
+
+if a % b == 0:
+    print("Your number is even!")
+else:
+    # Printing the remainder of 5 divided by 2 with the modulus operator, lit.
+    print(a % b)
