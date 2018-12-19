3287ebb6b0f3dc3dd4b9d83e04d5bcbd7b6a76c3
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 19:03:28 -0800

    complete challenge 3

--- Chapter 3. Introduction to Programming/challenge_03.py
+++ Chapter 3. Introduction to Programming/challenge_03.py
@@ -2,3 +2,12 @@
 # than or equal to 10, another message if the variable is greater
 # than 10 but less than or equal to 25, and another message if the
 # variable is greater than 25.
+
+num = 26
+
+if num <= 10:
+    print("Your input is less than 10 yo.")
+elif num <= 25:
+    print("Your number is between 10 and 25 and or the numbers indicated in this string.")    
+else:
+    print("Your number is greater than 25.")
