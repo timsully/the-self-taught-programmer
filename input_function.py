52d17f0297cbe551250f6c94b57de05eb7641697
Tim Sullivan <timsullivan97@gmail.com>
2019-01-02 19:05:01 -0800

    refactor exercise

--- input_function.py
+++ input_function.py
@@ -1,28 +1,15 @@
 # Ask the user to print a number three times. Then, an if-else statement
 # checks if the number is even. If so, it prints n is even.
+def even_odd():
+  n = input("type a number:")
+  n = int(n)
+  if n % 2 == 0:
+    print("n is even")
+  else:
+    print("n is odd")
 
 
-n = input("type a number:")
-n = int(n)
-
-
-if n % 2 == 0:
-  print("n is even")
-else:
-  print("n is odd")
-
-
-n = input("type a number:")
-n = int(n)
-if n % 2 == 0:
-  print("n is even")
-else:
-  print("n is odd")
-
-
-n = input("type a number:")
-n = int(n)
-if n % 2 == 0:
-  print("n is even")
-else:
-  print("n is odd")
+# telling function even_odd to execute three times
+even_odd()
+even_odd()
+even_odd()
