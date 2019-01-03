83504d9daa540539bd4a422cc2f73daa6f997448
Tim Sullivan <timsullivan97@gmail.com>
2019-01-02 18:54:35 -0800

    add chapter 4

---
+++ Chapter 4. Functions/functions.py
@@ -0,0 +1,67 @@
+# Function syntax (python)
+# def [function_name]([parameters]):
+#     [function_definition]
+
+#def f(x):
+#  return x * 2
+#f(8)
+
+def f(x):
+  return x + 1
+
+z = f(4)
+
+if z == 5:
+  print("z is 5")
+else:
+  print("z is not 5")
+
+
+
+# If a function doesn't have a return statement it will return "None"
+def f():
+  z = 1 + 1
+
+result = f()
+print(result)
+
+
+# len returns the length of an object
+len("Monty")
+
+
+# str function takes an object and returns a new object with a str data type
+str(100)
+
+
+# int function takes an object and returns an integer object
+int("1")
+
+
+# float function takes an object and returns a floating-point number object
+float(100)
+
+
+# input is a built-in function that collects information from the person
+# using the program
+age = input("Enter your age:")
+int_age = int(age)
+if int_age < 21:
+  print("You are young!")
+else:
+  print("Wow, you are old!")
+
+
+
+# prints whether x is even or odd
+def even_odd(x):
+  if x % 2 == 0
+    print("even")
+  else:
+    print("odd")
+
+even_odd(2)
+even_odd(3)
+
+
+
