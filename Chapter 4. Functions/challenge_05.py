993610494d0e70ec3596ece3f724075eb77303e4
Tim Sullivan <timsullivan97@gmail.com>
2019-01-03 19:54:32 -0800

    complete challenge 5

--- Chapter 4. Functions/challenge_05.py
+++ Chapter 4. Functions/challenge_05.py
@@ -1,2 +1,10 @@
 # Write a function that converts a string to a float and returns the result
 # Use exception handling to catch the exception that could occur.
+def string_to_float(x):
+  try:
+    return float(x)
+  except ValueError:
+    print("Must be a number in string format to convert value to float.")
+
+switch_this = string_to_float("80")
+print(switch_this)
